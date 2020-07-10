3b6fefd998d7afd33d873e1d5e4d5c67c3263647
quocan <t.q.a412@gmail.com>
2020-07-10 09:21:38 +0700

    tao heaher va footer

---
+++ Laravel_Webgame_1.1/resources/views/block/footer.blade.php
@@ -0,0 +1,129 @@
+<footer class="page-footer bg-dark text-light font-small mdb-color pt-4">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">ALA Game</h6>
+        <p>Trang web chơi game online #1 tại Việt Nam. <br> Hãy đến với chúng tôi và trải nghiệm.</p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+        <p>
+            <a href="#!">MDBootstrap</a>
+        </p>
+        <p>
+            <a href="#!">MDWordPress</a>
+        </p>
+        <p>
+            <a href="#!">BrandFlow</a>
+        </p>
+        <p>
+            <a href="#!">Bootstrap Angular</a>
+        </p>
+        </div>
+        <!-- Grid column -->
+
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+        <p>
+            <a href="#!">Your Account</a>
+        </p>
+        <p>
+            <a href="#!">Become an Affiliate</a>
+        </p>
+        <p>
+            <a href="#!">Shipping Rates</a>
+        </p>
+        <p>
+            <a href="#!">Help</a>
+        </p>
+        </div>
+
+        <!-- Grid column -->
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Liên hệ</h6>
+        <p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4033.296553418758!2d106.62738836414012!3d10.85131975974659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752a2769eec849%3A0xc623b505cddae0bf!2zQ2FvIMSR4bqzbmcgVmnhu4VuIMSQw7RuZw!5e0!3m2!1svi!2s!4v1594344286585!5m2!1svi!2s" width="400" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </p>
+        <p>
+            <i class="fa fa-envelope mr-3"></i>
+            <a href="mailto:support@ala.com" type="email">support@ala.com</a>
+        </p>
+        <p>
+            <a href="tellto:+84999999999">+ 84 999 999 999</a>
+        </p>
+       
+        </div>
+        <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+        <!-- Grid column -->
+        <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2020 ALA Game Copyright:
+            <a href="https://mdbootstrap.com/">
+            <strong> MDBootstrap.com</strong>
+            </a>
+        </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+            <ul class="list-unstyled list-inline">
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-sm rgba-white-slight mx-1" href="#">
+                    <img src="{{ asset('img/icons8-facebook-96.png') }}"   width="45"" height="45"  alt="">
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-sm rgba-white-slight mx-1" href="#">
+                        <img src="{{ asset('img/icons8-twitter-96.png') }}"   width="45"" height="45"  alt="">
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a class="btn-floating btn-sm rgba-white-slight mx-1" href="#">
+                        <img src="{{ asset('img/icons8-google-96.png') }}"   width="45"" height="45"  alt="">
+                    </a>
+                </li>                
+            </ul>
+        </div>
+
+        </div>
+        <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+</footer>
---
+++ Laravel_Webgame_1.1/resources/views/block/head.blade.php
@@ -0,0 +1,9 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="{{ asset( 'css/style.css') }}">
+<link rel="stylesheet" href="{{ asset( 'css/bootstrap.min.css') }}">
+<script src="{{ asset( 'js/jquery-3.5.1.slim.min.js') }}"></script>
+<script src="{{ asset( 'js/popper.min.js') }}"></script>
+<script src="{{ asset( 'js/bootstrap.min.js') }}"></script>
+<title>@yield('title','Home page')</title>
